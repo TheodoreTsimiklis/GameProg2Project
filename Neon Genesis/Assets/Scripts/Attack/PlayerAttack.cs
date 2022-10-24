0aa1bd36c763052ff2e3f8d35c29004b48900cd2
marcoagostinelli <2034731@edu.vaniercollege.qc.ca>
2022-10-24 19:16:40 -0400

    completed simple combat mechanism
    sword makes sound when swung,
    chance for critical hit once enemy is hit,
    enemy disappears when at 0 health

--- Neon Genesis/Assets/Scripts/Attack/PlayerAttack.cs
+++ Neon Genesis/Assets/Scripts/Attack/PlayerAttack.cs
@@ -4,45 +4,39 @@ using UnityEngine;
 
 public class PlayerAttack : MonoBehaviour
 {
-    int movementSpeed = 5;
+    public GameObject weapon;
     Animator[] sword;
-    AudioSource weapon;
+    AudioSource hitSound;
+    public bool isAttacking = false;
     // Start is called before the first frame update
     void Start()
     {
-        sword = GetComponentsInChildren<Animator>();
-        weapon = GetComponentInChildren<AudioSource>();
+        sword = weapon.GetComponents<Animator>();
+        hitSound = weapon.GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
-    {
-        if (Input.GetKey(KeyCode.A))
-            GetComponent<Rigidbody>().AddForce(new Vector3(-movementSpeed,0,0));
-        if (Input.GetKey(KeyCode.D))
-            GetComponent<Rigidbody>().AddForce(new Vector3(movementSpeed,0,0));
-        if (Input.GetKey(KeyCode.W))
-            GetComponent<Rigidbody>().AddForce(new Vector3(0,0,movementSpeed));
-        if (Input.GetKey(KeyCode.S))
-            GetComponent<Rigidbody>().AddForce(new Vector3(0,0,-movementSpeed));    
+    {   
 
         if (Input.GetKeyDown(KeyCode.Mouse0)){
 
            SwordAttack();
-           Debug.Log("hit");
         }
             
     }
     public void SwordAttack()
     {
+        isAttacking = true;
         //activates the trigger in the animator
         sword[0].SetTrigger("Hit");
-        if (!weapon.isPlaying)
+        if (!hitSound.isPlaying)
         {
-            weapon.Play();
-        } 
+            hitSound.Play();
+        }
 
     }
+
 }
 
     
