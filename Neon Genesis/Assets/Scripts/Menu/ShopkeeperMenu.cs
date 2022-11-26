3b5bff5d64013214df4941a5e76e689a7f960e51
Reimar Rosas <reimar@reimarrosas.me>
2022-11-25 22:11:48 -0500

    (#55) Implemented Stats and Purchase Systems, Issue #40
    * Implemented Stats and Purchase Systems
    
    - Implemented a way to `Level Up` the stats when purchasing in the the
      shop
    - Implemented the money system that allows a player to purchase stat
      upgrades
    
    * Removed the preprocessor commands
    
    - Removed the preprocessor commands in PlayerStats since it doesn't really
      work.
    
    Note: Don't forget to remove the `P` binding on prod though.
    
    * Used observer pattern to notify menu for stat changes
    
    - Created Observer, an interface to be used for listening to PlayerStat
      changes
    - Created Subject, an interface to to be assigned to PlayerStat and to
      be used to add observers for stat changes
    - Refactored ShopKeeperMenu and PlayerStats to use the Observer and
      Subject interface respectively

--- Neon Genesis/Assets/Scripts/Menu/ShopkeeperMenu.cs
+++ Neon Genesis/Assets/Scripts/Menu/ShopkeeperMenu.cs
@@ -1,16 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
-public class ShopkeeperMenu : MonoBehaviour
+public class ShopkeeperMenu : MonoBehaviour, Observer
 {
+    private readonly static Color DisabledButton = new Color(0, ByteColorToFloat(90), ByteColorToFloat(125), ByteColorToFloat(150));
+    private readonly static Color EnabledButton = new Color(0, ByteColorToFloat(174), ByteColorToFloat(242), ByteColorToFloat(150));
+    private readonly static Color InsufficientText = new Color(ByteColorToFloat(255), ByteColorToFloat(81), ByteColorToFloat(13), ByteColorToFloat(255));
+
     [SerializeField]
     private GameObject m_Player;
     private PlayerStats m_PlayerStats;
 
-    void Start()
+    private bool purchased = false;
+
+    private int BasePrice => 200;
+
+    [Header("Upgrade Prompts")]
+    [SerializeField]
+    private TextMeshProUGUI AttackPrompt;
+    [SerializeField]
+    private TextMeshProUGUI CritPrompt;
+    [SerializeField]
+    private TextMeshProUGUI HealthPrompt;
+    [SerializeField]
+    private TextMeshProUGUI SpeedPrompt;
+
+    [Header("Price Prompts")]
+    [SerializeField]
+    private TextMeshProUGUI AttackPrice;
+    [SerializeField]
+    private TextMeshProUGUI CritPrice;
+    [SerializeField]
+    private TextMeshProUGUI HealthPrice;
+    [SerializeField]
+    private TextMeshProUGUI SpeedPrice;
+    [SerializeField]
+    private TextMeshProUGUI PlayerMoney;
+
+    [Header("Purchase Buttons")]
+    [SerializeField]
+    private GameObject AttackPurchaseButton;
+    [SerializeField]
+    private GameObject CritPurchaseButton;
+    [SerializeField]
+    private GameObject HealthPurchaseButton;
+    [SerializeField]
+    private GameObject SpeedPurchaseButton;
+
+    void Awake()
     {
         m_PlayerStats = m_Player.GetComponent<PlayerStats>();
+        m_PlayerStats.attach(this);
+        SetMenuState();
     }
 
     public void ExitMenu()
@@ -25,32 +69,122 @@ public class ShopkeeperMenu : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape)) {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
             ExitMenu();
         }
     }
 
     public void BuyAttack()
     {
-        Debug.Log("Bought Attack");
-        // Handler to increase player attack
+        int attackLevel = m_PlayerStats.AttackLevel;
+        Purchase(attackLevel);
+        m_PlayerStats.AttackLevelUp();
+        Debug.Log($"Attack Damage: {m_PlayerStats.AttackDamage}");
     }
 
     public void BuyCrit()
     {
-        Debug.Log("Bought Crit");
-        // Handler to increase player crit
+        int critLevel = m_PlayerStats.CritLevel;
+        Purchase(critLevel);
+        m_PlayerStats.CritLevelUp();
+        Debug.Log($"Crit Chance: {m_PlayerStats.CritChance * 100}%");
     }
 
     public void BuyHealth()
     {
-        Debug.Log("Bought Health");
-        // Handler to increase player health
+        int healthLevel = m_PlayerStats.HealthLevel;
+        Purchase(healthLevel);
+        m_PlayerStats.HealthLevelUp();
+        Debug.Log($"Max Health: {m_PlayerStats.MaxHealth}");
     }
 
     public void BuySpeed()
     {
-        Debug.Log("Bought Speed");
-        // Handler to increase player speed
+        int speedLevel = m_PlayerStats.SpeedLevel;
+        Purchase(speedLevel);
+        m_PlayerStats.SpeedLevelUp();
+        Debug.Log($"Speed Multiplier: {m_PlayerStats.Speed}");
+    }
+
+    private void SetButtonState(GameObject button, int statLevel)
+    {
+        var buttonImage = button.GetComponent<Image>();
+        var buttonButton = button.GetComponent<Button>();
+        if (IsPurchaseable(statLevel))
+        {
+            buttonImage.color = EnabledButton;
+            buttonButton.interactable = true;
+        }
+        else
+        {
+            buttonImage.color = DisabledButton;
+            buttonButton.interactable = false;
+        }
+    }
+
+    private void SetPriceState(TextMeshProUGUI text, int statLevel)
+    {
+        if (IsPurchaseable(statLevel))
+        {
+            text.color = Color.white;
+        }
+        else
+        {
+            text.color = InsufficientText;
+        }
+
+        text.text = $"{CalculatePrice(statLevel)} G";
+    }
+
+    private void Purchase(int statLevel)
+    {
+        m_PlayerStats.Money -= CalculatePrice(statLevel);
+    }
+
+    private void SetPromptsState()
+    {
+        int attackLevel = m_PlayerStats.AttackLevel;
+        int critLevel = m_PlayerStats.CritLevel;
+        int healthLevel = m_PlayerStats.HealthLevel;
+        int speedLevel = m_PlayerStats.SpeedLevel;
+
+        AttackPrompt.text = $"{m_PlayerStats.AttackScaling(attackLevel)}→{m_PlayerStats.AttackScaling(attackLevel + 1)}";
+        CritPrompt.text = $"{m_PlayerStats.CritScaling(critLevel) * 100}%→{m_PlayerStats.CritScaling(critLevel + 1) * 100}%";
+        HealthPrompt.text = $"{m_PlayerStats.HealthScaling(healthLevel)}→{m_PlayerStats.HealthScaling(critLevel + 1)}";
+        SpeedPrompt.text = $"{m_PlayerStats.SpeedScaling(speedLevel)}→{m_PlayerStats.SpeedScaling(speedLevel + 1)}";
+
+        SetPriceState(AttackPrice, attackLevel);
+        SetPriceState(CritPrice, critLevel);
+        SetPriceState(HealthPrice, healthLevel);
+        SetPriceState(SpeedPrice, speedLevel);
+        PlayerMoney.text = $"{m_PlayerStats.Money} G";
+    }
+
+    private void SetButtonsState()
+    {
+        SetButtonState(AttackPurchaseButton, m_PlayerStats.AttackLevel);
+        SetButtonState(CritPurchaseButton, m_PlayerStats.CritLevel);
+        SetButtonState(HealthPurchaseButton, m_PlayerStats.HealthLevel);
+        SetButtonState(SpeedPurchaseButton, m_PlayerStats.SpeedLevel);
+    }
+
+    private void SetMenuState()
+    {
+        SetPromptsState();
+        SetButtonsState();
+    }
+
+    private bool IsPurchaseable(int statLevel) => m_PlayerStats.Money >= CalculatePrice(statLevel);
+
+    // Probably change this into a quadratic scaling later on.
+    private int CalculatePrice(int statLevel) => (int)((.25f * statLevel + 1) * BasePrice);
+
+
+    private static float ByteColorToFloat(byte color) => color / 255f;
+
+    public void SubjectUpdate()
+    {
+        SetMenuState();
     }
 }
--- Neon Genesis/Assets/Scripts/Player/PlayerStats.cs
+++ Neon Genesis/Assets/Scripts/Player/PlayerStats.cs
@@ -1,18 +1,83 @@
 using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 
-public class PlayerStats : MonoBehaviour
+public class PlayerStats : MonoBehaviour, Subject
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public int AttackDamage { get; private set; }
+    public float CritChance { get; private set; }
+    public int MaxHealth { get; private set; }
+    public float Speed { get; private set; }
 
-    // Update is called once per frame
+    public int AttackLevel { get; private set; } = 0;
+    public int CritLevel { get; private set; } = 0;
+    public int HealthLevel { get; private set; } = 0;
+    public int SpeedLevel { get; private set; } = 0;
+
+    public int Money { get; set; } = 1000;
+
+    private List<Observer> observers = new List<Observer>();
+
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            Money = 1000;
+            notify();
+            Debug.Log("Player given 1000 G");
+        }
+    }
+
+    // NOTE: Probably change the scaling from Linear to Logarithmic later on.
+    public void AttackLevelUp()
+    {
+        ++AttackLevel;
+        AttackDamage = AttackScaling(AttackLevel);
+        notify();
+    }
+
+    public void CritLevelUp()
+    {
+        ++CritLevel;
+        CritChance = CritScaling(CritLevel);
+        notify();
+    }
+    public void HealthLevelUp()
+    {
+        ++HealthLevel;
+        MaxHealth = HealthScaling(HealthLevel);
+        notify();
+    }
+    public void SpeedLevelUp()
+    {
+        ++SpeedLevel;
+        Speed = SpeedScaling(SpeedLevel);
+        notify();
+    }
+
+    public int AttackScaling(int level) => (int)StatScaling(1.1f, level, 10f);
+    public float CritScaling(int level) => StatScaling(0.02f, level, 0.05f);
+    public int HealthScaling(int level) => (int)StatScaling(5f, level, 200f);
+    public float SpeedScaling(int level) => StatScaling(0.01f, level, 1f);
+
+    private float StatScaling(float scale, int level, float baseVal) => scale * level + baseVal;
+
+    public void attach(Observer o)
+    {
+        observers.Add(o);
+    }
+
+    public void detach(Observer o)
+    {
+        observers.Remove(o);
+    }
+
+    public void notify()
+    {
+        foreach (var o in observers)
+        {
+            o.SubjectUpdate();
+        }
     }
 }
