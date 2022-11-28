60b7016611c898028fb20d62d231ce287dd26467
marcoagostinelli <70527916+marcoagostinelli@users.noreply.github.com>
2022-11-28 17:05:58 -0500

    Enemy animations marco #62 (#63)
    * Implemented death animations for enemies
    
    * Fixed  enemy health bar face camera

--- Neon Genesis/Assets/Scripts/Enemies/HealthbarFacePlayer.cs
+++ Neon Genesis/Assets/Scripts/Enemies/HealthbarFacePlayer.cs
@@ -4,11 +4,8 @@ using UnityEngine;
 
 public class HealthbarFacePlayer : MonoBehaviour
 {
-    public Transform cam;
-
-    
     void LateUpdate()
     {
-        transform.LookAt(cam);
+        transform.rotation = Camera.main.transform.rotation;
     }
 }
